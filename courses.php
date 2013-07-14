31d86eb7af2fd746ca7f2b2668dd7767f70f0dc3
Shanshan Zhou <sszhou@live.cn>
2013-07-15 00:35:41 +1200

    fixed course information

--- courses.php
+++ courses.php
@@ -1,87 +1,127 @@
 <?php include('header.php'); ?>
 
 <div id="courses">                
-             <div class="level" id="level_1">
+   <div class="level" id="level_1">
+	<h3> BDI | BSc<br/>Undergraduate</h3>
 				<h2 class="level">100 LEVEL COURSES</h2>
 				<div class= "course_type" id ="cg_core">
-				<h3>CG Core Courses</h3>
 					<div class = "course">
 						<p class="course_name"><span class = "course_code">DSDN 142</span><br/>Creative Coding</p>
-						<p class="course_description">Research Interests: Computer vision and statistical learning applied to graphics, design and composition from a computational point of view.</p>		
-						<a href=""><p class="more">Read more &rarr;</p></a>
+						<p class="course_description">This course introduces the core topics of Interaction Design (as well as other disciplines) are motion, interaction and procedures (interconnected processes). </p>		
+						<a href="http://www.victoria.ac.nz/design/study/courses/dsdn-142"><p class="more">Read more &rarr;</p></a>
 					</div>
 					<div class = "course">
 						<p class="course_name"><span class = "course_code">COMP 102</span><br/> Intro to Computer Program Design</p>
-						<p class="course_description">Research Interests: Computer vision and statistical learning applied to graphics, design and composition from a computational point of view.</p>		
-						<a href=""><p class="more">Read more &rarr;</p></a>
+						<p class="course_description">This course introduces the fundamentals of programming in a high-level programming language (Java), using an object oriented approach to program design. </p>
+						<a href="http://www.victoria.ac.nz/home/study/subjects/coursecatalogue.aspx?course=COMP-102&t=_2012&res=d"><p class="more">Read more &rarr;</p></a>
 					</div>
 					<div class = "course">
 						<p class="course_name"><span class = "course_code">COMP 103</span><br/>Intro to Data Structures and Algorithms</p>
-						<p class="course_description">Research Interests: Computer vision and statistical learning applied to graphics, design and composition from a computational point of view.</p>		
-						<a href=""><p class="more">Read more &rarr;</p></a>
-					</div>
-				</div>
-				<div class= "course_type" id ="cg_design">
-				<h3>CG Design Courses</h3>
-					<div class = "course">
-						<p class="course_name"><span class = "course_code">DSDN 112</span><br/>INTRODUCTION TO INTERACTION DESIGN</p>
-						<p class="course_description">Research Interests: Computer vision and statistical learning applied to graphics, design and composition from a computational point of view.</p>		
-						<a href=""><p class="more">Read more &rarr;</p></a>
+						<p class="course_description">This course builds on COMP 102, focusing on the techniques for designing, building and analysing computer programs that deal with large collections of data. </p>
+						<a href="http://www.victoria.ac.nz/home/study/subjects/coursecatalogue.aspx?course=COMP-103&t=_2012&res=d"><p class="more">Read more &rarr;</p></a>
 					</div>
 				</div>
 				<div class= "course_type" id ="cg_comp">
-				<h3>CG Computer Science Courses</h3>
 					<div class = "course">
-						<p class="course_name"><span class = "course_code">STAT 193</span><br/>Stats for Natural and Social Sciences</p>
-						<p class="course_description">Research Interests: Computer vision and statistical learning applied to graphics, design and composition from a computational point of view.</p>		
+						<p class="course_name"><span class = "course_code">MATH 151</span><br/>Algebra</p>
+						<p class="course_description">This course inctroduces linear algebra, including matrices and vectors, complex numbers, eigenvectors and algebraic structures.</p><a href="http://www.victoria.ac.nz/design/study/courses/dsdn-112"><p class="more">Read more &rarr;</p></a>
+					</div>
+					<div class = "course">
+						<p class="course_name"><span class = "course_code">MATH 161 </span><br/> Discrete Mathematics and Logic</p>
+						<p class="course_description">This course is an introduction to mathematical logic, proofs, sets and relations; basic number theory and polynomials; graphs, trees and networks.</p>
 						<a href=""><p class="more">Read more &rarr;</p></a>
 					</div>
 				</div>
             </div>
                 
-			<div class="level" id="level_2">
+	<div class="level" id="level_2">
 				<h2 class="level">200 LEVEL COURSES</h2>
-					<div class= "course_type" id ="cg_core">
-					<h3>CG Core Courses</h3>
-					<div class = "course" >
+					<div class= "course_type" id ="cg_design">
+						<div class = "course" >
 							<p class="course_name"><span class = "course_code">MDDN 241</span><br/>3D MODELING AND ANIMATION<br/> </p>
 							<p class="course_description">Research Interests: Computer vision and statistical learning applied to graphics, design and composition from a computational point of view.</p>		
 							<a href=""><p class="more">Read more &rarr;</p></a>
-						</div>
+						</div>	
 						<div class = "course" >
 							<p class="course_name"><span class = "course_code">MDDN 242</span><br/>COMPUTER GRAPHICS PRODUCTION </p>
-							<p class="course_description">Research Interests: Computer vision and statistical learning applied to graphics, design and composition from a computational point of view.</p>		
-							<a href=""><p class="more">Read more &rarr;</p></a>
+							<p class="course_description">This course explores topics including parameterized design, generative modelling, and essential elements of creative 2D and 3D coding.</p>
+							<a href="http://www.victoria.ac.nz/design/study/courses/mddn-242"><p class="more">Read more &rarr;</p></a>
 						</div>
+					</div>
+					
+					<div class= "course_type" id ="cg_comp">
 						<div class = "course" >
 							<p class="course_name"><span class = "course_code">COMP 261</span><br/>Algorithms and Data Structures</p>
-							<p class="course_description">Research Interests: Computer vision and statistical learning applied to graphics, design and composition from a computational point of view.</p>		
-							<a href=""><p class="more">Read more &rarr;</p></a>
+							<p class="course_description">This course covers a range of algorithms and data structures building on the fundamental structures and algorithms from COMP 103.</p>
+							<a href="http://www.victoria.ac.nz/design/study/courses/mddn-242"><p class="more">Read more &rarr;</p></a>
 						</div>	
 					</div>
-					<div class= "course_type" id ="cg_design">
-					<h3>CG Design Courses</h3>
-						<div class = "course" >
-							<p class="course_name"><span class = "course_code">CCDN 271</span><br/>Design as Inquiry</p>
-							<p class="course_description">Research Interests: Computer vision and statistical learning applied to graphics, design and composition from a computational point of view.</p>		
-							<a href=""><p class="more">Read more &rarr;</p></a>
-						</div>						
-					</div>
-					<!--
-					<div class= "course_type" id ="cg_comp">
-					<h3>CG Computer Science Courses</h3>
-						<div class = "course" >
-							<p class="course_name">Algorithms and Data Structures |<br/> <span class = "course_code">COMP 261</span></p>
-							<p class="course_description">Research Interests: Computer vision and statistical learning applied to graphics, design and composition from a computational point of view.</p>		
-							<a href=""><p class="more">Read more &rarr;</p></a>
-						</div>		
-					</div>
-					-->
                 </div>
 		
-				<img class="line_4" src="images/line_2.png" alt="line" height="1" width="820" />
-
-
-	    </div>
+	<div class="level" id="level_3">
+				<h2 class="level">300 LEVEL COURSES</h2>
+					<div class= "course_type" id ="cg_design">
+						<div class = "course" >
+							<p class="course_name"><span class = "course_code">MDDN 311</span><br/>Postproduction and Special Effects</p>
+							<p class="course_description">This course focuses on digital media products such as film special effects and games often require the creation of novel visual experiences while working within large professional graphics software packages.</p>
+							<a href="http://www.victoria.ac.nz/design/study/courses/mddn-311"><p class="more">Read more &rarr;</p></a>
+						</div>	
+					</div>
+					
+					<div class= "course_type" id ="cg_comp">
+						<div class = "course" >
+							<p class="course_name"><span class = "course_code">COMP 308</span><br/>Introduction to Computer Graphics</p>
+							<p class="course_description">This course give introduction to graphics programming. Graphics APIs, in particular OpenGL. Graphics processing pipeline (geometry processing, viewing, projection, transformation, illumination, texture mapping). Display hardware. </p>
+							<a href="http://www.victoria.ac.nz/home/study/subjects/coursecatalogue.aspx?course=COMP-308&t=_2012&res=d"><p class="more">Read more &rarr;</p></a>
+						</div>	
+					</div>
+                </div>                
+    <div class="level" id="level_4">                	
+             	<h3> MDI | MSc<br/>Postgraduate</h3>
+				<h2 class="level">400 LEVEL COURSES</h2>
+					<div class= "course_type" id ="cg_design">
+						<div class = "course" >
+							<p class="course_name"><span class = "course_code">MDDN 441</span><br/>Computer Graphics for Film</p>
+							<p class="course_description">This course covers computer graphics techniques that are used as current practice in the film industry.</p>
+							<a href="http://www.victoria.ac.nz/design/study/courses/mddn-441"><p class="more">Read more &rarr;</p></a>
+						</div>	
+						<div class = "course" >
+							<p class="course_name"><span class = "course_code">MDDN 442</span><br/>Computer Graphics for Interaction Design</p>
+							<p class="course_description">This course examines computer graphics techniques that are current practice in interactive computer graphics.</p>
+							<a href="http://www.victoria.ac.nz/design/study/courses/mddn-442"><p class="more">Read more &rarr;</p></a>
+						</div>	
+					</div>
+					
+					<div class= "course_type" id ="cg_comp">
+						<div class = "course" >
+							<p class="course_name"><span class = "course_code">COMP 408</span><br/>Computer Graphics Rendering</p>
+							<p class="course_description">This course covers a range of topics i.e. Ray tracing. Advanced lighting techniques. Texture mapping and its derivatives. Radiosity. Photon mapping. Advanced shading and lighting techniques on modern graphics cards.</p>
+							<a href="http://www.victoria.ac.nz/home/study/subjects/coursecatalogue.aspx?course=COMP-408&t=_2012&res=d"><p class="more">Read more &rarr;</p></a>
+						</div>	
+						<div class = "course" >
+							<p class="course_name"><span class = "course_code">COMP 409</span><br/>Three-Dimensional Modelling for Computer Graphics</p>
+						<p class="course_description">This course explores a selection of modelling techniques and applications from polygon meshes, point clouds, B-Splines subdivision surfaces, computational geometry, implicit surfaces, voxels, particle systems and animation.</p>
+							<a href="http://www.victoria.ac.nz/home/study/subjects/coursecatalogue.aspx?course=COMP-409&t=_2012&res=d"><p class="more">Read more &rarr;</p></a>
+						</div>	
+					</div>
+                </div>
+    <div class="level" id="level_5">
+				<h2 class="level">500 LEVEL COURSES</h2>
+					<div class= "course_type" id ="cg_design">
+						<div class = "course" >
+							<p class="course_name"><span class = "course_code">MDDN 594</span><br/> Thesis in Computer Graphics</p>
+							<p class="course_description">Supervised research undertaken in association with structured colloquia series.</p>
+							<a href="http://www.victoria.ac.nz/home/study/subjects/coursecatalogue.aspx?course=mddn-594"><p class="more">Read more &rarr;</p></a>
+						</div>	
+					</div>					
+					<div class= "course_type" id ="cg_comp">
+						<div class = "course" >
+							<p class="course_name"><span class = "course_code">COMP 595</span><br/> Thesis in Computer Graphics</p>
+							<p class="course_description">Supervised research undertaken in association with structured colloquia series.</p>
+							<a href="http://www.victoria.ac.nz/home/study/subjects/coursecatalogue.aspx?course=mddn-594"><p class="more">Read more &rarr;</p></a>
+						</div>	
+					</div>
+                </div>
+</div>
 <?php include('return.php'); ?>
 <?php include('footer.php'); ?>
