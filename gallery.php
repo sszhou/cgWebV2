f1e4e246658627df02eea0740cb065206e782623
Shanshan Zhou <sszhou@live.cn>
2013-07-25 18:35:14 +1200

    adding 308 gallery

--- gallery.php
+++ gallery.php
@@ -2,60 +2,96 @@
 <script type="text/javascript" src="js/gallery.js"></script>
 
 	<div class="module" id="gallery">
+		<h2>Student works</h2>
 	    <div id="featured_works">  
 	    	<div class="batch">
-             	<h3>MDDN 311<br/>Student Works</h3>
+             	<h3>MDDN 311</h3>
 	    		<div class="thumbs">
              	<div class="view pic">
                     <a src="images/gallery/311/guerin-harmonious-full.jpg" alt ="Francis Guerin<br/>Harmonious Discord">
                          <img src="images/gallery/311/guerin-harmonious-thumb.jpg" alt="" height="244" width="400" />
                     </a>
-                     <p>Francis Guerin<br/>Harmonious Discord</p>
+                     <p>Harmonious Discord<br/>Francis Guerin</p>
                  </div>
                  <div class="view pic">
                     <a src="images/gallery/311/callus-6-full.png" alt ="Chris Callus<br/>6">
                          <img src="images/gallery/311/callus-6-thumb.png" alt="" height="244" width="400" />
                     </a>
-                     <p>Chris Callus<br/>6</p>
+                     <p>6<br/>Chris Callus</p>
                  </div>
                  <div class="view pic">
                     <a src="images/gallery/311/fong-argia-full.png" alt ="Rosanna Fong<br/>Argia">
                          <img src="images/gallery/311/fong-argia-thumb.png" alt="" height="244" width="400" />
                      </a>
-                      <p>Rosanna Fong<br/>Argia</p>
+                      <p>Argia<br/>Rosanna Fong</p>
                  </div>
                  <div class="view pic">
                     <a src="images/gallery/311/hartley-valdrada-full.png" alt ="Ben Hartley<br/>Valdrada">
                      <img src="images/gallery/311/hartley-valdrada-thumb.png" alt="" height="244" width="400" />
                      </a>
-                     <p>Ben Hartley<br/>Valdrada</p>
+                     <p>Valdrada</br>Ben Hartley</p>
                  </div>
                  <div class="view pic">
                     <a src="images/gallery/311/taylor-ersilia-full.png" alt ="Emma Taylor<br/>Ersilia">
                      <img src="images/gallery/311/taylor-ersilia-thumb.png" alt="" height="244" width="400" />
                      </a>
-                     <p>Emma Taylor<br/>Ersilia</p>
+                     <p>Ersilia<br/>Emma Taylor</p>
                  </div>
                  <div class="view pic">
                     <a src="images/gallery/311/zhou_cecilia-full.png" alt ="Shanshan Zhou<br/>Cecilia">
                      <img src="images/gallery/311/zhou_cecilia-thumb.png" alt="" height="244" width="400" />
                      </a>
-                      <p>Shanshan Zhou<br/>Cecilia</p>
+                      <p>Cecilia<br/>Shanshan Zhou</p>
+                 </div>
+              </div>
+             </div>
+             <div class="batch">
+             	<h3>COMP 308</h3>
+	    		<div class="thumbs">
+             	<div class="view vid">
+                    <a  id ="70995676" src="images/gallery/308/Andrew_full.jpg" alt ="COMP 308<br/>Andrew Chalmers">
+                         <img src="images/gallery/308/Andrew.jpg" alt="" height="244" width="400" />
+                    </a>
+                     <p>Fast collision control using octree structure<br/>Andrew Chalmers</p>
+                 </div>
+                 <div class="view vid">
+                    <a id ="70995678" src="images/gallery/308/Cameron_full.jpg" alt ="COMP 308<br/>Cameron Owen">
+                         <img src="images/gallery/308/Cameron.jpg" alt="" height="244" width="400" />
+                    </a>
+                     <p>Facial deformation using geodesic radial basis functions<br/>Cameron Owen</p>
+                 </div>
+                 <div class="view vid">
+                    <a id ="70995679" src="images/gallery/308/Melby_Mattew_full.jpg" alt ="COMP 308<br/>Melby Ruarus, Matthew Rollitt">
+                         <img src="images/gallery/308/Melby_Mattew.jpg" alt="" height="244" width="400" />
+                    </a>
+                     <p>Realistic real-time rendering for large scene model<br/>Melby Ruarus & Matthew Rollitt</p>
+                 </div>
+                 <div class="view vid">
+                    <a id ="70995902" src="images/gallery/308/Thomas_Mat_Aneta_full.jpg" alt ="COMP 308<br/>Thomas Caskey, Matthew Betts, Aneta Stevanovic">
+                         <img src="images/gallery/308/Thomas_Mat_Aneta.jpg" alt="" height="244" width="400" />
+                    </a>
+                     <p>Particle simulation and animation using B-spline <br/>Thomas Caskey, Matthew Betts, Aneta Stevanovic</p>
+                 </div>
+                  <div class="view vid">
+                    <a id ="70996474" src="images/gallery/308/Jaron_full.jpg" alt ="COMP 308<br/>Jaron Peek">
+                         <img src="images/gallery/308/Jaron.jpg" alt="" height="244" width="400" />
+                    </a>
+                     <p>Jaron Peek<br/>particle simulation</p>
                  </div>
               </div>
              </div>
              
              <div class="batch">
-             	<h3>COMP 408<br/>Student Works</h3>
+             	<h3>COMP 408</h3>
 	    		<div class="thumbs">
-             	<div class="view pic">
-                    <a src="images/gallery/408/group1.jpg" alt ="COMP 408<br/>Andrew Chalmers, Ian Loh &  Kieran Carnegie">
+             	<div class="view vid">
+                    <a  id ="70917302" src="images/gallery/408/group1.jpg" alt ="COMP 408<br/>Andrew Chalmers, Ian Loh &  Kieran Carnegie">
                          <img src="images/gallery/408/group1.jpg" alt="" height="244" width="400" />
                     </a>
                      <p>This image is a composition of virtual objects into a photograph. Can you distinguish which objects were rendered?</p>
                  </div>
-                 <div class="view pic">
-                    <a src="images/gallery/408/group2.jpg" alt ="COMP 408<br/> Melby Ruarus & Alistair St Pierre">
+                 <div class="view vid">
+                    <a id ="70917348" src="images/gallery/408/group2.jpg" alt ="COMP 408<br/> Melby Ruarus & Alistair St Pierre">
                          <img src="images/gallery/408/group2.jpg" alt="" height="244" width="400" />
                     </a>
                      <p>This image is a composition of virtual objects into a photograph. Can you distinguish which objects were rendered?</p>
@@ -64,7 +100,7 @@
              </div>
              
 	    	<div class="batch">
-             <h3>MDI Postgraduate<br/>Student Works</h3>
+             <h3>MDI Postgraduate</h3>
              <div class="thumbs">
 
                  <div class="view pic">
@@ -89,7 +125,7 @@
                      <a id ="62297998" src="images/gallery/masters/mallett-1-thumb.jpg" alt ="Byron Mallett<br/>Tech Bytes">
                      	<img src="images/gallery/masters/mallett-1-thumb.jpg" alt="" height="244" width="400" />
                      </a>             
-                      <p>Byron Mallett<br/>Tech Bytes</p>
+                      <p>Tech Bytes<br/>Byron Mallett</p>
                  </div>
 				</div>
              </div>
@@ -97,13 +133,13 @@
         	<div id="preview">         
         		<div class="btn-wrap"><a href="#" class="btn" id = "play">Play</a></div> 
               	<img src="images/gallery/311/guerin-harmonious-full.jpg" />
-	        	<p class ="caption">Francis Guerin<br/>Harmonious Discord</p>
+	        	<p class ="caption">Harmonious Discord<br/>Francis Guerin</p>
 	        </div> 
 	        <div class="fullview">
 	        	<div id="framing">
 	        	<img src="images/gallery/311/guerin-harmonious-full.jpg" />
 	        	</div>
-	        	<iframe src = "http://player.vimeo.com/video/62297998?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff" width="800" height = "500" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+	        	<iframe src = "http://player.vimeo.com/video/62297998?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff" width="800" height = "400" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
              	<p></p>
              	<div class="btn-wrap"><a href="#" class="btn" id = "close">Close</a></div> 
              </div>
